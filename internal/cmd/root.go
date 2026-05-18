7bb2a27fba1209f71526e7959b45fb0de564363f
Ivan Han <xhanwei@gmail.com>
2026-05-18 16:28:39 +0800

    feat: change '--help' flag output to human user friendly ,add number index indicator

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -3,9 +3,19 @@ package cmd
 import (
 	"fmt"
 	"log/slog"
+	"sort"
 
 	"github.com/spf13/cobra"
 )
+
+var cmdDisplayOrder = map[string]int{
+	"doctor":     0,
+	"new":        1,
+	"integrate":  2,
+	"serve":      3,
+	"status":     4,
+	"renew-cert": 5,
+}
 
 type BuildInfo struct {
 	Version   string
@@ -48,14 +58,91 @@ func NewRootCommand(logLevel *slog.LevelVar, info BuildInfo) *cobra.Command {
 	root.Flags().BoolVarP(&verbose, "verbose", "v", false, "enable detailed diagnostic logging to stderr (redacted)")
 	root.Flags().BoolVarP(&showVersion, "version", "V", false, "print version information")
 
-	root.AddCommand(
-		newNewCommand(info.Version),
-		newIntegrateCommand(),
-		newServeCommand(info.Version),
-		newStatusCommand(info.Version),
-		newDoctorCommand(),
-		newRenewCertCommand(info.Version),
+	root.AddGroup(
+		&cobra.Group{ID: "1-diagnose", Title: "1. Diagnose:"},
+		&cobra.Group{ID: "2-configure", Title: "2. Configure:"},
+		&cobra.Group{ID: "3-run", Title: "3. Run:"},
+		&cobra.Group{ID: "4-inspect", Title: "4. Inspect:"},
+		&cobra.Group{ID: "5-maintain", Title: "5. Maintain:"},
 	)
 
+	cobra.AddTemplateFunc("sortByOrder", func(cmds []*cobra.Command) []*cobra.Command {
+		sorted := make([]*cobra.Command, len(cmds))
+		copy(sorted, cmds)
+		sort.Slice(sorted, func(i, j int) bool {
+			oi, oki := cmdDisplayOrder[sorted[i].Name()]
+			oj, okj := cmdDisplayOrder[sorted[j].Name()]
+			if !oki {
+				oi = 999
+			}
+			if !okj {
+				oj = 999
+			}
+			return oi < oj
+		})
+		return sorted
+	})
+
+	root.SetUsageTemplate(`Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
+
+Available Commands:{{range (sortByOrder $cmds)}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
+
+{{.Title}}{{range (sortByOrder $cmds)}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
+
+	newCmd := newNewCommand(info.Version)
+	newCmd.GroupID = "2-configure"
+
+	integrateCmd := newIntegrateCommand()
+	integrateCmd.GroupID = "2-configure"
+
+	serveCmd := newServeCommand(info.Version)
+	serveCmd.GroupID = "3-run"
+
+	statusCmd := newStatusCommand(info.Version)
+	statusCmd.GroupID = "4-inspect"
+
+	doctorCmd := newDoctorCommand()
+	doctorCmd.GroupID = "1-diagnose"
+
+	renewCmd := newRenewCertCommand(info.Version)
+	renewCmd.GroupID = "5-maintain"
+
+	root.AddCommand(doctorCmd, newCmd, integrateCmd, serveCmd, statusCmd, renewCmd)
+
+	root.CompletionOptions.HiddenDefaultCmd = true
+
+	root.SetHelpTemplate(helpTemplate())
+
 	return root
+}
+
+func helpTemplate() string {
+	return `{{with (or .Long .Short)}}{{. | trimTrailingWhitespaces}}
+
+{{end}}{{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}{{if eq .Name "warded"}}
+
+Tip: First time? Run ` + "`warded doctor --preflight`" + ` to verify this host is ready.{{end}}
+`
 }
